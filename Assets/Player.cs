9cd5b4a14ba6b323bbbe70a1a73b5fbf61beb33f
Alan.Stewart@port.ac.uk <Alan.Stewart@port.ac.uk>
2025-08-07 12:13:46 +0100

    Old input handling add on

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float speed = 5f;
+
     private void Update()
     {
-        float xMove = Input.GetAxis("Horizontal");
-        float yMove = Input.GetAxis("Vertical");
-        Debug.Log("xMove value:" + xMove);
-        Debug.Log("yMove value:" + yMove);
+        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(xMove, yMove, 0);
     }
 }
