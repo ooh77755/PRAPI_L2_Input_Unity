ec6a7eb7608897bcf96d9fc21def9a7342cb2ef4
ALAN_PC\alans <alanstewart07@hotmail.com>
2025-08-26 10:51:07 +0100

    Buggy directional rotation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,11 +5,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
-
+    float rotZ = 0f;
+    
     private void Update()
     {
         float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(xMove, yMove, 0);
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rotZ = 90f;
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            rotZ = -90f;
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            rotZ = 180f;
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            rotZ = 0f;
+            
+        }
+        transform.localRotation = Quaternion.Euler(0, 0, rotZ);
+
+
     }
+
 }
